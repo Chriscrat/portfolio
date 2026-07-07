79a3a294b6e5c396adc87b1a2bed8192891869ab
Christophe Barreto <dev-cba@pm.me>
2026-07-07 18:23:30 +0200

    chore: add robots option on sitemap

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import sitemap from 'vite-plugin-sitemap';
 export default defineConfig({
     plugins: [
         react(),
-        sitemap({ hostname: 'https://portfolio.dev-cba.com' })
+        sitemap({
+            hostname: 'https://portfolio.dev-cba.com',
+            robots: [{ userAgent: '*', allow: '/' }],
+        }),
     ],
     server: {
         proxy: {
